1eab1abea9ade9ba91a9e0c9d7c0997b19173c7f
Mendes <andremendes0113@gmail.com>
2024-05-09 03:40:23 -0300

    Refactor add_block method in Blockchain struct

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,5 +1,5 @@
 
-use chrono::Utc;
+
 
 use crate::block::Block;
 
@@ -20,7 +20,7 @@ impl Blockchain {
     //adicionar um bloco à blockchain
     pub fn add_block(&mut self, data: String) {
         let previous_hash = self.blocks.last().unwrap().hash.clone();
-        let new_block = Block::new((self.blocks.len() as u32), data, previous_hash);
+        let new_block = Block::new(self.blocks.len() as u32, data, previous_hash);
         self.blocks.push(new_block);
     }
 
