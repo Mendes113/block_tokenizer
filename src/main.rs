406318523974410f6400ac013c419f0ae19e3ca2
mendes113 <andremendes0113@gmail.com>
2024-05-31 14:42:23 -0300

    fix: :bug: troca de mensagens funcionando, blockchain data produz corretamente a mensagem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate chrono;
 extern crate crypto;
 extern crate serde;
@@ -25,33 +26,26 @@ lazy_static! {
 
 fn main() {
     env_logger::Builder::new()
-    .filter_level(LevelFilter::Info)
-    .init();
+        .filter_level(LevelFilter::Info)
+        .init();
     info!("Starting Rust blockchain...");
-   
 
     let consumer: BaseConsumer = ClientConfig::new()
         .set("group.id", "rust_blockchain_group")
-        .set("bootstrap.servers", "localhost:9092")
+        .set("bootstrap.servers", "127.0.0.1:9092")
         .create()
         .expect("Consumer creation failed");
 
     consumer.subscribe(&["blockchain"]).expect("Failed to subscribe to topic");
     info!("Subscribed to blockchain topic");
 
-
-
-   
-    
-  
     loop {
         match consumer.poll(Duration::from_millis(10)) {
             Some(Ok(message)) => {
                 if let Some(payload) = message.payload_view::<str>() {
                     match payload {
                         Ok(text) => {
-                           
-                            handle_message(text)
+                            handle_message(text);
                         },
                         Err(e) => error!("Error while deserializing message payload: {:?}", e),
                     }
@@ -61,41 +55,44 @@ fn main() {
             None => (),
         }
     }
+}
 
-    fn handle_message(message: &str) {
-        info!("Received message: {}", message); // Adiciona log de entrada
-    
-        let parts: Vec<&str> = message.split(':').collect();
-        if parts.len() == 2 && parts[0] == "add_block" {
-            let data = parts[1].to_string();
-            let difficulty = 2;  // Define a dificuldade conforme necessário
-    
-            let mut blockchain = BLOCKCHAIN.lock().unwrap();
-            match blockchain.add_block(data, difficulty) {
-                Ok(_) => {
-                    info!("Block added successfully");
-    
-                    // Configurar o Kafka Producer
-                    let producer: BaseProducer = ClientConfig::new()
-                        .set("bootstrap.servers", "localhost:9092")
-                        .create()
-                        .expect("Producer creation failed");
-    
-                    // Enviar mensagem para o Kafka
-                    info!("sending message to blockchain_data topic");
-                    producer.send(
-                   
-                        BaseRecord::to("blockchain_data")
-                            .key("blockchain_data")
-                            .payload("add_block:Hello, blockchain!")
-                    ).expect("Failed to send message");
-                },
-                Err(e) => error!("Failed to add block: {:?}", e),
-            }
+fn handle_message(message: &str) {
+    info!("Received message: {}", message); // Adiciona log de entrada
+
+    let parts: Vec<&str> = message.split(':').collect();
+    if parts.len() == 2 && parts[0] == "add_block" {
+        let data = parts[1].to_string();
+        let difficulty = 2;  // Define a dificuldade conforme necessário
+
+        let mut blockchain = BLOCKCHAIN.lock().unwrap();
+        match blockchain.add_block(data, difficulty) {
+            Ok(_) => {
+                info!("Block added successfully");
+
+                // Configurar o Kafka Producer
+                let producer: BaseProducer = ClientConfig::new()
+                    .set("bootstrap.servers", "127.0.0.1:9092")
+                    .create()
+                    .expect("Producer creation failed");
+
+                // Enviar mensagem para o Kafka
+                info!("Sending message to blockchain_data topic");
+                let result = producer.send(
+                    BaseRecord::to("blockchain_data")
+                        .key("blockchain_data")
+                        .payload("add_block:Hello, blockchain!")
+                );
+
+                match result {
+                    Ok(_) => {
+                        info!("Message sent successfully");
+                        producer.poll(Duration::from_millis(10));
+                    },
+                    Err(e) => error!("Failed to send message: {:?}", e),
+                }
+            },
+            Err(e) => error!("Failed to add block: {:?}", e),
         }
     }
-    
-    
-
-    
 }
